83b3ec8034537444cac8fc50c43af46d7c8c2aac
Cheikh Bassirou Syll <bassco70@gmail.com>
2023-01-31 01:35:15 +0100

    rendre dynamique l'affichage
    Co-authored-by: Amdy Dioum <amdy.dioum@univ-thies.sn>
    Signed-off-by: Cheikh Bassirou Syll <bassco70@gmail.com>

---
+++ src/timetable/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from timetable.models import Creneau
+
+
+class CreatePlanningForm(forms.ModelForm):
+    class Meta:
+        model = Creneau
+        exclude = ['date_debut', 'date_fin']
+
+        labels = {
+            'filiere': 'Filière',
+            'prof': 'Professeur'
+        }
+    date_de_debut = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+    date_de_fin = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
